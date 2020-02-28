24df986c6e969c0d6194b19f656a3b057f2cd77a
Wave1994-Hoon <kk03187693@gmail.com>
2020-02-28 15:42:37 +0900

    remove secret key

--- myfaketrip/settings.py
+++ myfaketrip/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mei2^lxyvvogxwm4xi83e#dv(ru+)_-3pxfhgibahywjpbj368'
+# SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
